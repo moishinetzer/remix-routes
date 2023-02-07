1add8a39d31d32fe36b99695791bf0ff49275b15
Wei Zhu <yesmeck@gmail.com>
2023-02-07 12:49:40 +1030

    feature: exports route definitions

--- e2e/app/routes/index.tsx
+++ e2e/app/routes/index.tsx
@@ -1,5 +1,16 @@
 import { $params, $path } from 'remix-routes';
 
+import { Routes } from 'remix-routes';
+import { redirect as remixRedirect } from "@remix-run/node";
+
+function redirect<R extends keyof Routes, P extends Routes[R]["params"], Q extends Routes[R]["query"]> (route: R, params: P, query?: Q) {
+  return remixRedirect(
+      $path(route as any, params, query)
+    );
+}
+
+redirect("/:lang?/about", { lang: 1 });
+
 $path('/posts/:id', { id: 1 }, { view: 'list', sort: 'price' });
 
 $path('/', { foo: 'bar' });
